61c475ad9c205375b10b36992166f568edd66a3c
OverseerY <yaroslavakimenko@gmail.com>
2018-03-13 16:48:39 +0600

    Added: Request permissions for READ_PHONE_STATE (get IMEI) and ACCESS_FINE_LOCATION. Changed types of NFC tags that can be read

--- app/src/main/java/com/yaroslav/materialapptest/WorkingActivity.java
+++ app/src/main/java/com/yaroslav/materialapptest/WorkingActivity.java
@@ -9,6 +9,8 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.tech.IsoDep;
 import android.nfc.tech.MifareClassic;
@@ -18,7 +20,10 @@ import android.nfc.tech.NfcA;
 import android.nfc.tech.NfcB;
 import android.nfc.tech.NfcV;
 import android.os.AsyncTask;
-import android.os.Handler;
+import android.os.Parcelable;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -31,31 +36,20 @@ import android.widget.Toast;
 
 import com.google.gson.GsonBuilder;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class WorkingActivity extends AppCompatActivity {
@@ -69,11 +63,16 @@ public class WorkingActivity extends AppCompatActivity {
     private String description = "";
     private String tId = "88888";
 
+    private View mLayout;
+
     private String mLatitude = null;
     private String mLongitude = null;
     private String curTime = null;
 
     private static final String TAG = "TESTGPS";
+
+    private static final int PERMISSION_REQUEST_IMEI = 0;
+    private static final int PERMISSION_REQUEST_LOCATION = 0;
 
     private static final int LOCATION_INTERVAL = 1000;
     private static final float LOCATION_DISTANCE = 1f;
@@ -94,6 +93,7 @@ public class WorkingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_working);
+        mLayout = findViewById(R.id.main_layout);
 
         output_text = (TextView) findViewById(R.id.output_text);
         output_text.setMovementMethod(new ScrollingMovementMethod());
@@ -125,13 +125,35 @@ public class WorkingActivity extends AppCompatActivity {
 
     @Override
     protected void onNewIntent(Intent intent) {
+        String s = "";
+        // parse through all NDEF messages and their records and pick text type only
+        Parcelable[] data = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        if (data != null) {
+            try {
+                for (int i = 0; i < data.length; i++) {
+                    NdefRecord[] recs = ((NdefMessage) data[i]).getRecords();
+                    for (int j = 0; j < recs.length; j++) {
+                        if (recs[j].getTnf() == NdefRecord.TNF_WELL_KNOWN &&
+                                Arrays.equals(recs[j].getType(), NdefRecord.RTD_TEXT)) {
+                            byte[] payload = recs[j].getPayload();
+                            String textEncoding = ((payload[0] & 0200) == 0) ? "UTF-8" : "UTF-16";
+                            int langCodeLen = payload[0] & 0077;
+
+                            s += (new String(payload, langCodeLen + 1, payload.length - langCodeLen - 1, textEncoding) + "\n");
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                Log.e("TagDispatch", e.toString());
+            }
+        }
         if (intent.getAction().equals(NfcAdapter.ACTION_TAG_DISCOVERED)) {
             if (mLatitude != null && mLongitude != null) {
                 String temp = output_text.getText().toString();
                 String location = "" + mLatitude + "; " + mLongitude + "; " + curTime;
                 String nfcid = ByteArrayToHexString(intent.getByteArrayExtra(NfcAdapter.EXTRA_ID));
                 description = nfcid + "; " + location;
-                output_text.setText(temp + nfcid + "; " + location + "\n" );
+                output_text.setText(temp + nfcid + ", " + s + "; " + location + "\n" );
                 new UploadJsonTask().execute();
             } else {
                 String temp = output_text.getText().toString();
@@ -178,23 +200,27 @@ public class WorkingActivity extends AppCompatActivity {
 
     private void initializeLocationProvider() {
         Log.e(TAG, "initializeLocationProvider");
-        try {
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE, locationListener);
-        } catch (java.lang.SecurityException ex) {
-            Log.i(TAG, "Fail to request location update, ignore", ex);
-            Toast.makeText(this, "Fail to request location update", Toast.LENGTH_SHORT).show();
-        } catch (IllegalArgumentException ex) {
-            Log.d(TAG, "Network provider does not exist, " + ex.getMessage());
-            Toast.makeText(this, "Network provider does not exist", Toast.LENGTH_SHORT);
-        }
-        try {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE, locationListener);
-        } catch (java.lang.SecurityException ex) {
-            Log.i(TAG, "Fail to request location update, ignore", ex);
-            Toast.makeText(this, "Fail to request location update", Toast.LENGTH_SHORT).show();
-        } catch (IllegalArgumentException ex) {
-            Log.d(TAG, "Gps provider does not exist " + ex.getMessage());
-            Toast.makeText(this, "Gps provider does not exist", Toast.LENGTH_SHORT).show();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            try {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE, locationListener);
+            } catch (java.lang.SecurityException ex) {
+                Log.i(TAG, "Fail to request location update, ignore", ex);
+                Toast.makeText(this, "Fail to request location update", Toast.LENGTH_SHORT).show();
+            } catch (IllegalArgumentException ex) {
+                Log.d(TAG, "Network provider does not exist, " + ex.getMessage());
+                Toast.makeText(this, "Network provider does not exist", Toast.LENGTH_SHORT);
+            }
+            try {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE, locationListener);
+            } catch (java.lang.SecurityException ex) {
+                Log.i(TAG, "Fail to request location update, ignore", ex);
+                Toast.makeText(this, "Fail to request location update", Toast.LENGTH_SHORT).show();
+            } catch (IllegalArgumentException ex) {
+                Log.d(TAG, "Gps provider does not exist " + ex.getMessage());
+                Toast.makeText(this, "Gps provider does not exist", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            requestLocationPermissions();
         }
     }
 
@@ -216,13 +242,72 @@ public class WorkingActivity extends AppCompatActivity {
 
     //==============================================================================================
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_IMEI) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Snackbar.make(mLayout, R.string.imei_granted, Snackbar.LENGTH_SHORT).show();
+                IMEI = getImei();
+            } else {
+                Snackbar.make(mLayout, R.string.imei_denied, Snackbar.LENGTH_SHORT).show();
+            }
+        }
+        if (requestCode == PERMISSION_REQUEST_LOCATION) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Snackbar.make(mLayout, R.string.imei_granted, Snackbar.LENGTH_SHORT).show();
+            } else {
+                Snackbar.make(mLayout, R.string.imei_denied, Snackbar.LENGTH_SHORT).show();
+            }
+        }
+    }
+
     private String getImei() {
         String imei = null;
         if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
             TelephonyManager telephonyManager = (TelephonyManager) getSystemService(getApplicationContext().TELEPHONY_SERVICE);
             imei = telephonyManager.getDeviceId();
+        } else {
+            requestImeiPermission();
         }
         return imei;
+    }
+
+    private void requestLocationPermissions() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Snackbar.make(mLayout, R.string.location_permission_required, Snackbar.LENGTH_INDEFINITE).setAction(R.string.ok, new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ActivityCompat.requestPermissions(WorkingActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_LOCATION);
+                }
+            }).show();
+        } else {
+            Snackbar.make(mLayout, R.string.location_unavailable, Snackbar.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_LOCATION);
+        }
+    }
+
+    private void requestImeiPermission() {
+        // Permission has not been granted and must be requested.
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_PHONE_STATE)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // Display a SnackBar with cda button to request the missing permission.
+            Snackbar.make(mLayout, R.string.imei_permission_required,
+                    Snackbar.LENGTH_INDEFINITE).setAction(R.string.ok, new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Request the permission
+                    ActivityCompat.requestPermissions(WorkingActivity.this,
+                            new String[]{Manifest.permission.READ_PHONE_STATE}, PERMISSION_REQUEST_IMEI);
+                }
+            }).show();
+
+        } else {
+            Snackbar.make(mLayout, R.string.imei_unavailable, Snackbar.LENGTH_SHORT).show();
+            // Request the permission. The result will be received in onRequestPermissionResult().
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, PERMISSION_REQUEST_IMEI);
+        }
     }
 
     public static void executeJson(String id, String name, String imei) {
